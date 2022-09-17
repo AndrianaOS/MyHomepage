6d23f3706c601cd23b5183e825107f051c26101d
AndrianaOS <113118206+AndrianaOS@users.noreply.github.com>
2022-09-17 15:29:25 +0100

    Update script.js

--- script.js
+++ script.js
@@ -77,9 +77,24 @@ var click_count;
 click_count = 0;
 
 
-document.getElementById('high_five_btn').addEventListener('click', (event) => {
+document.getElementById('cheesecake_button').addEventListener('click', (event) => {
   click_count = (typeof click_count === 'number' ? click_count : 0) + 1;
   let element_click_counter = document.getElementById('click-counter');
   element_click_counter.innerText = click_count;
+
+});  
+  
+  
+var click_count;
+
+
+click_count = 0;
+
+
+document.getElementById('brownies_button').addEventListener('click', (event) => {
+  click_count = (typeof click_count === 'number' ? click_count : 0) + 1;
+  let element_click_counter = document.getElementById('click-counter');
+  element_click_counter.innerText = click_count;
+
 
 });
