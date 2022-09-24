4fec02329d347419b1d7da6b121992eb96b10e67
AndrianaOS <113118206+AndrianaOS@users.noreply.github.com>
2022-09-24 13:03:41 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,26 @@
-var click_count;
+var click_cheesecake, click_brownies;
 
 
-click_count = 0;
 
+document.getElementById('click_count').addEventListener('click', (event) => {
+  click_count = (typeof click_count === 'number' ? click_count : 0) + 1;
+  event.target.innerText = click_count;
+
+});
 
 document.getElementById('cheesecake_button').addEventListener('click', (event) => {
-  click_count = (typeof click_count === 'number' ? click_count : 0) + 1;
-  let element_click_counter = document.getElementById('click-counter');
-  element_click_counter.innerText = click_count;
+  click_cheesecake = (typeof click_cheesecake === 'number' ? click_cheesecake : 0) + 1;
+  let element_click_cheesecake = document.getElementById('click-cheesecake');
+  element_click_cheesecake.innerText = click_cheesecake;
 
-});  
-  
-  
-var click_count;
-
-
-click_count = 0;
+});
+click_cheesecake = 0;
+click_brownies = 0;
 
 
 document.getElementById('brownies_button').addEventListener('click', (event) => {
-  click_count = (typeof click_count === 'number' ? click_count : 0) + 1;
-  let element_click_counter = document.getElementById('click-counter');
-  element_click_counter.innerText = click_count;
-
+  click_brownies = (typeof click_brownies === 'number' ? click_brownies : 0) + 1;
+  let element_click_brownies = document.getElementById('click-brownies');
+  element_click_brownies.innerText = click_brownies;
 
 });
