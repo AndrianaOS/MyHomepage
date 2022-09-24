e6303d17aa20ef85002b70a40899fd484036e6d2
AndrianaOS <113118206+AndrianaOS@users.noreply.github.com>
2022-09-24 21:27:18 +0100

    Update script.js
    update for shopping list and checklist

--- script.js
+++ script.js
@@ -38,3 +38,102 @@ document.getElementById('previous').addEventListener('click', (event) => {
   element_images3.setAttribute("src", cheesecake.pop());
 
 });
+
+var shop_list, to_do_list, item;
+
+
+
+document.getElementById('addbread').addEventListener('click', (event) => {
+  let element_current = document.getElementById('current');
+  let new_li = document.createElement('li');
+  new_li.innerText = 'Bread';
+
+  element_current.appendChild(new_li);
+
+});
+
+document.getElementById('addmilk').addEventListener('click', (event) => {
+  let element_current2 = document.getElementById('current');
+  let new_li2 = document.createElement('li');
+  new_li2.innerText = 'Milk';
+
+  element_current2.appendChild(new_li2);
+
+});
+
+document.getElementById('addtea').addEventListener('click', (event) => {
+  let element_current3 = document.getElementById('current');
+  let new_li3 = document.createElement('li');
+  new_li3.innerText = 'Tea';
+
+  element_current3.appendChild(new_li3);
+
+});
+shop_list = ['Eggs', 'Bacon', 'Sausages'];
+to_do_list = ['clean house', 'wash dishes', 'do shopping'];
+let element_shopping_list = document.getElementById('shopping-list');
+shop_list.forEach((item) => {
+  let new_li4 = document.createElement('li');
+  new_li4.innerText = item;
+  let new_span = document.createElement('span');
+  let new_button = document.createElement('button');
+  new_button.innerText = 'add to cart';
+  new_button.setAttribute("id", item);
+
+  new_span.appendChild(new_button);
+
+  new_li4.appendChild(new_span);
+
+  element_shopping_list.appendChild(new_li4);
+});
+let element_to_do = document.getElementById('to-do');
+to_do_list.forEach((item) => {
+  let new_li5 = document.createElement('li');
+  new_li5.innerText = item;
+  new_li5.setAttribute("id", item);
+
+  element_to_do.appendChild(new_li5);
+});
+
+
+document.getElementById('Eggs').addEventListener('click', (event) => {
+  let element_current4 = document.getElementById('current');
+  let new_li6 = document.createElement('li');
+  new_li6.innerText = 'Eggs';
+
+  element_current4.appendChild(new_li6);
+
+});
+
+document.getElementById('Bacon').addEventListener('click', (event) => {
+  let element_current5 = document.getElementById('current');
+  let new_li7 = document.createElement('li');
+  new_li7.innerText = 'Bacon';
+
+  element_current5.appendChild(new_li7);
+
+});
+
+document.getElementById('Sausages').addEventListener('click', (event) => {
+  let element_current6 = document.getElementById('current');
+  let new_li8 = document.createElement('li');
+  new_li8.innerText = 'Sausages';
+
+  element_current6.appendChild(new_li8);
+
+});
+
+document.getElementById('wash dishes').addEventListener('click', (event) => {
+  event.target.style.backgroundColor = '#000000';
+
+});
+
+document.getElementById('clean house').addEventListener('click', (event) => {
+  event.target.style.backgroundColor = '#000000';
+
+});
+
+document.getElementById('do shopping').addEventListener('click', (event) => {
+  event.target.style.backgroundColor = '#000000';
+
+});
